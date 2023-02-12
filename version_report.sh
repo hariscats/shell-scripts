30237762a01c50c1bb9335def4129f5b22f1e194
Haris Catakovic <hariscats@gmail.com>
2023-02-12 22:06:27 +0000

    Check version of SW and email

---
+++ version_report.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+HostIpAddress=$(hostname -i)
+NginxVersion=$(nginx -v 2>&1 | cut -d " " -f 3 | cut -d "/" -f 2)
+DockerVersion=$(docker -v | cut -d " " -f3 | tr "," " ")
+OSUname=$(uname)
+EmailAddress=$(ENTER_EMAI_ADDRESS)
+
+cat <<EOF | /usr/bin/mail -s "$(date): Report on $(hostname)" $(EmailAddress)"
+Docker Version: $DockerVersion
+Nginx Version: $NginxVersion
+OS Name: $OSUname
+EOF
