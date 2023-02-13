0b3a474ce64bb220c71d6e94a47d47447c40741a
hariscats <hariscats@gmail.com>
2023-02-13 12:58:30 -0500

    Update formatting.

--- version_report.sh
+++ version_report.sh
@@ -4,7 +4,7 @@ HostIpAddress=$(hostname -i)
 NginxVersion=$(nginx -v 2>&1 | cut -d " " -f 3 | cut -d "/" -f 2)
 DockerVersion=$(docker -v | cut -d " " -f3 | tr "," " ")
 OSUname=$(uname)
-EmailAddress=$(ENTER_EMAI_ADDRESS)
+EmailAddress=$(ENTER_EMAIL_ADDRESS)
 
 cat <<EOF | /usr/bin/mail -s "$(date): Report on $(hostname)" $(EmailAddress)"
 Docker Version: $DockerVersion
