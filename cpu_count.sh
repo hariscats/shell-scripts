56d646c86c72286ec61a68f09745aa913ee98d68
hariscats <hariscats@gmail.com>
2023-02-17 12:44:43 -0500

    Create cpu_count.sh

---
+++ cpu_count.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Given confusion between CPUs and vCPUs, this script provides a count of physical CPUs, logical CPUs and number of cores per physical CPU
+
+CPUFILE=/proc/cpuinfo
+test -f $CPUFILE || exit 1
+
+echo "Physical CPUs: $(grep "physical id" $CPUFILE | sort -u | wc -l)"
+echo "Logical Cores: $(grep "core id" $CPUFILE | sort -u | wc -l)"
+echo "CPU count:     $(grep "processor" $CPUFILE | wc -l)"
