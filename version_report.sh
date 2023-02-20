ab5455362af4df2c386a5cea2d2038a4b690c474
hariscats <hariscats@gmail.com>
2023-02-19 20:50:57 -0500

    Remove mail command; make portable

--- version_report.sh
+++ version_report.sh
@@ -1,12 +1,11 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 HostIpAddress=$(hostname -i)
 NginxVersion=$(nginx -v 2>&1 | cut -d " " -f 3 | cut -d "/" -f 2)
 DockerVersion=$(docker -v | cut -d " " -f3 | tr "," " ")
 OSUname=$(uname)
-EmailAddress=$(ENTER_EMAIL_ADDRESS)
 
-cat <<EOF | /usr/bin/mail -s "$(date): Report on $(hostname)" $(EmailAddress)"
+cat <<EOF
 Docker Version: $DockerVersion
 Nginx Version: $NginxVersion
 OS Name: $OSUname
