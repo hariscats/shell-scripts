5dcc4cbd4abe1456ea3267eb35222b3931a72e8b
hariscats <hariscats@gmail.com>
2023-02-26 10:19:09 -0500

    Add comments

--- azure/search_activity_logs.sh
+++ azure/search_activity_logs.sh
@@ -1,12 +1,15 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-# Search activity logs for historical access permissions for custom roles
+# Search activity logs for historical access patterns
+# Useful for building custom roles
 
-start_time="2023-02-22"
-time_offset="6h" # Use "7d" for specifying days
+start_time="2023-02-22"          # Enter a date
+time_offset="6h"                 # Use "7d" format for specifying days
 
 az monitor activity-log list \
            --start-time $start_time \
            --offset $time_offset \
            | jq -r '.[] | .operationName["value"] ' \
            | sort -u
+
+exit 0
