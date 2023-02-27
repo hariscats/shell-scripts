e1d7c78898928b5ed7e0b9d372524fdf42861ba9
hariscats <hariscats@gmail.com>
2023-02-27 14:37:10 -0500

    Create nginx_operator.sh

---
+++ kubernetes/nginx_operator.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+# Operator that creates a Deployment when there's a new ConfigMap
+
+kubectl get --watch --output-watch-events configmap \
+-o=custom-columns=type:type,name:object.metadata.name \
+--no-headers | \
+while read next; do                                        
+
+    NAME=$(echo $next | cut -d' ' -f2)                     
+    EVENT=$(echo $next | cut -d' ' -f1)
+
+    case $EVENT in
+        ADDED|MODIFIED)                                    
+            kubectl apply -f - << EOF
+apiVersion: apps/v1
+kind: Deployment
+metadata: { name: $NAME }
+spec:
+  selector:
+    matchLabels: { app: $NAME }
+  template:
+    metadata:
+      labels: { app: $NAME }
+      annotations: { kubectl.kubernetes.io/restartedAt: $(date) }
+    spec:
+      containers:
+      - image: nginx
+        name: $NAME
+        ports:
+        - containerPort: 80
+        volumeMounts:
+        - { name: data, mountPath: /usr/share/nginx/html }
+      volumes:
+      - name: data
+        configMap:
+          name: $NAME
+EOF
+            ;;
+        DELETED)                                            #E
+            kubectl delete deploy $NAME
+            ;;
+    esac
+done
