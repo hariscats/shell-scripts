3b82657c84affc0b35cb8555ed6bb2a736c99490
Haris Catakovic <hariscats@gmail.com>
2023-12-27 16:20:58 -0500

    Update install-az-cli.sh for robustness

--- install-az-cli.sh
+++ install-az-cli.sh
@@ -1,18 +1,54 @@
 #!/bin/bash
 
-# Add Microsoft package repository
-sudo apt-get update
-sudo apt-get install -y ca-certificates curl apt-transport-https lsb-release gnupg
-curl -sL https://packages.microsoft.com/keys/microsoft.asc | 
-    gpg --dearmor | 
-    sudo tee /etc/apt/trusted.gpg.d/microsoft.asc.gpg > /dev/null
-AZ_REPO=$(lsb_release -cs)
-echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" | 
-    sudo tee /etc/apt/sources.list.d/azure-cli.list
+set -e
 
-# Install Azure CLI
-sudo apt-get update
-sudo apt-get install -y azure-cli
+# Function to ensure a command exists
+ensure_command() {
+    if ! command -v $1 &> /dev/null
+    then
+        echo "Error: $1 is not installed. Exiting."
+        exit 1
+    fi
+}
 
-# Verify installation
-az --version
+# Ensure necessary commands are available
+ensure_command curl
+ensure_command gpg
+ensure_command lsb_release
+ensure_command apt-get
+
+add_microsoft_repo() {
+    echo "Adding Microsoft package repository..."
+
+    # Add the Microsoft package signing key
+    curl -sL https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/microsoft.asc.gpg > /dev/null
+
+    # Add the Azure CLI software repository
+    AZ_REPO=$(lsb_release -cs)
+    echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" | sudo tee /etc/apt/sources.list.d/azure-cli.list
+
+    # Update package lists
+    sudo apt-get update
+}
+
+install_azure_cli() {
+    echo "Installing Azure CLI..."
+    sudo apt-get install -y azure-cli
+}
+
+verify_installation() {
+    echo "Verifying Azure CLI installation..."
+    az --version
+}
+
+# Main execution
+if [ "$EUID" -ne 0 ]
+    then echo "Please run as root or use sudo"
+    exit
+fi
+
+add_microsoft_repo
+install_azure_cli
+verify_installation
+
+echo "Azure CLI installation completed successfully."
